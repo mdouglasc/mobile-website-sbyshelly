3ac8f07d9e5587f776248f9d1601b24e50768e91
mdouglasc <miche.doug29@gmail.com>
2024-12-16 12:11:28 -0400

    Update sunkissed.js

--- sunkissed.js
+++ sunkissed.js
@@ -69,10 +69,7 @@ function clearError(input) {
 
 // Toggle the menu when the hamburger icon is clicked
 function toggleMenu() {
-    const menu = document.querySelector('.navbar__menu');
-    menu.classList.toggle('active');
+    const menu = document.querySelector('.off-screen-menu');
+    menu.classList.toggle('active'); // Toggle 'active' class
 }
-
-// Add event listener to the hamburger icon
-document.querySelector('.hamburger-menu').addEventListener('click', toggleMenu);
 
